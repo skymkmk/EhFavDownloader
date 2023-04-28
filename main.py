2e9534b2644baf1f5619235defe56a18f0d3c281
skymkmk <admin@skymkmk.com>
2023-04-28 23:00:47 +0800

    feat: now can only download imgs rather than update fav info via args

--- main.py
+++ main.py
@@ -2,18 +2,21 @@
 import asyncio
 import datetime
 import os.path
+import sys
 
 from loguru import logger
 
 from src import *
 
 if __name__ == '__main__':
+    args = sys.argv[1:]
     working_dir = os.path.split(__file__)[0]
     if not os.path.exists(os.path.join(working_dir, 'logs')):
         os.mkdir(os.path.join(working_dir, 'logs'))
     logger.add(os.path.join(working_dir, "logs",
                             f"{datetime.datetime.strftime(datetime.datetime.now(), '%Y-%m-%d')}.log"), level="WARNING")
     init()
-    update_data()
+    if not ('--download-only' in args or '-d' in args):
+        update_data()
     asyncio.run(download())
     logger.success("Downloaded finished. Program will exit.")
