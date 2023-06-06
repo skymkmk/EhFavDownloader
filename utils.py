836cda5be31b90a5c6bf5fdc8d47fb8318c804d7
skymkmk <admin@skymkmk.com>
2023-06-06 18:41:20 +0800

    refactor: split the path trancate from download.py to util.py

--- utils.py
+++ utils.py
@@ -1,9 +1,27 @@
+import os
 import time
+from sys import platform
 
 import aiohttp
 from loguru import logger
 
 import config
+
+
+# Limit the folder name length to 255 (bytes for Linux, characters for Windows / macOS) and the path length to
+# 259 characters (Windows) or 1023 bytes (macOS) or 4095 bytes (Linux) due to the limit of file system.
+# Long path extensions supported in Windows 10 1607 and later will not be supported because Windows Explorer
+# does not support long paths and because of compatibility issues.
+os_brand = platform.system()
+if os_brand == 'Windows':
+    PATH_LENGTH_LIMIT = 259
+elif os_brand == 'Darwin':
+    PATH_LENGTH_LIMIT = 1023
+elif os_brand == 'Linux':
+    PATH_LENGTH_LIMIT = 4095
+else:
+    PATH_LENGTH_LIMIT = 4095
+FILE_NAME_LENGTH_LIMIT = 255
 
 
 class Error509(BaseException):
@@ -35,3 +53,22 @@ async def get(url: str, data: str = None, retry_time: int = config.retry_time,
                     return await get(url, data=data, ultimate_retry_time=ultimate_retry_time - 1)
                 else:
                     logger.error(f"Error to connect {url}. Skip it.")
+
+
+def truncate_path(root: str, file_name: str) -> str:
+    if os_brand in ['Windows', 'Darwin']:
+        if len(file_name) > FILE_NAME_LENGTH_LIMIT:
+            file_name = file_name[:FILE_NAME_LENGTH_LIMIT]
+    elif len(file_name.encode(encoding='utf-8')) > FILE_NAME_LENGTH_LIMIT:
+        file_name = file_name.encode(encoding='utf-8')[: FILE_NAME_LENGTH_LIMIT].decode(encoding='utf-8',
+                                                                                        errors='ignore')
+    if os_brand == 'Windows':
+        # Because the path does not contain / at the end, here subtract one more. The following is the same as here.
+        length_limit = PATH_LENGTH_LIMIT - len(root) - 1
+        if len(file_name) > length_limit:
+            file_name = file_name[: length_limit]
+    else:
+        length_limit = PATH_LENGTH_LIMIT - len(root.encode(encoding='utf-8')) - 1
+        if len(file_name.encode(encoding='utf-8')) > length_limit:
+            file_name = file_name.encode(encoding='utf-8')[: length_limit].decode(encoding='utf-8', errors='ignore')
+    return os.path.join(root, file_name)
