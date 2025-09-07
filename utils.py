bb49501f69e8688cf146069869109250e5da67b5
skymkmk <i@skymkmk.com>
2025-09-07 18:41:12 +0800

    perf(client): switch from aiohttp to httpx to avoid empty response body, and reuse the client rather than create it by a new request

--- utils.py
+++ utils.py
@@ -6,11 +6,28 @@ import platform
 import re
 import time
 from typing import Union
+from weakref import WeakKeyDictionary
 
-import aiohttp
+import httpx
 from loguru import logger
 
 import config
+
+headers = {
+    'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7',
+    'Accept-Encoding': 'gzip, deflate, br, zstd',
+    'Accept-Language': 'en-ZA,en;q=0.9',
+    'Connection': 'keep-alive',
+    'Sec-Fetch-Dest': 'document',
+    'Sec-Fetch-Mode': 'navigate',
+    'Sec-Fetch-Site': 'none',
+    'Sec-Fetch-User': '?1',
+    'Upgrade-Insecure-Requests': '1',
+    'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/140.0.0.0 Safari/537.36',
+    'sec-ch-ua': '"Chromium";v="140", "Not=A?Brand";v="24", "Google Chrome";v="140"',
+    'sec-ch-ua-mobile': '?0',
+    'sec-ch-ua-platform': '"Windows"'
+}
 
 # Limit the folder name length to 255 (bytes for Linux, characters for Windows / macOS) and the path length to
 # 259 characters (Windows) or 1023 bytes (macOS) or 4095 bytes (Linux) due to the limit of file system.
@@ -36,34 +53,42 @@ class Error509(BaseException):
 class FailToDownloadIMG(BaseException):
     pass
 
+_clients: WeakKeyDictionary = WeakKeyDictionary()
 
-async def get(url: str, data: str = None, retry_time: int = config.retry_time,
-              ultimate_retry_time: int = config.retry_time) -> Union[bytes, None]:
-    async with aiohttp.ClientSession(cookies=config.cookies, headers={'User-Agent': config.user_agent}) \
-            as session:
-        try:
-            if data is None:
-                async with session.get(url, proxy=config.proxy['url'] if config.proxy['enable'] else None) \
-                        as resp:
-                    return await resp.read()
-            else:
-                async with session.post(url, data=data,
-                                        proxy=config.proxy['url'] if config.proxy['enable'] else None) as resp:
-                    return await resp.read()
-        except BaseException:
-            if retry_time > 0:
-                logger.warning(f"Error to connect {url}. Retrying. Remaining retry counts: {retry_time}")
-                time.sleep(5)
-                return await get(url, data=data, retry_time=retry_time - 1, ultimate_retry_time=ultimate_retry_time)
-            else:
-                if ultimate_retry_time > 0:
-                    logger.warning(f"Error to connect {url}. Will sleep for 60 secs.")
-                    await asyncio.sleep(60)
-                    return await get(url, data=data, ultimate_retry_time=ultimate_retry_time - 1)
-                else:
-                    logger.error(f"Error to connect {url}. Skip it.")
-                    return
+async def _get_client() -> httpx.AsyncClient:
+    loop = asyncio.get_running_loop()
+    
+    if loop not in _clients:
+        _clients[loop] = httpx.AsyncClient(
+            cookies=config.cookies,
+            headers=headers,
+        )
+    
+    return _clients[loop]
 
+async def get(url: str, data: str = None, retry_time: int = 0,
+              ultimate_retry_time: int = 0) -> Union[bytes, None]:
+    client = await _get_client()
+    try:
+        if data is None:
+            resp = await client.get(url)
+        else:
+            resp = await client.post(url, data=data)
+        resp.raise_for_status()
+        return resp.content
+    except BaseException:
+        if retry_time > 0:
+            print(f"Error to connect {url}. Retrying. Remaining retry counts: {retry_time}")
+            await asyncio.sleep(5)
+            return await get(url, data=data, retry_time=retry_time - 1, ultimate_retry_time=ultimate_retry_time)
+        else:
+            if ultimate_retry_time > 0:
+                print(f"Error to connect {url}. Will sleep for 60 secs.")
+                await asyncio.sleep(60)
+                return await get(url, data=data, ultimate_retry_time=ultimate_retry_time - 1)
+            else:
+                print(f"Error to connect {url}. Skip it.")
+                return None
 
 def truncate_path(root: str, file_name: str, spare_limit: int = 10) -> str:
     file_name = ILLEGAL_NAME.sub('', file_name)
